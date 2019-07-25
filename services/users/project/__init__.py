67f318567fb86efa09d8b8a792b1f7c0492cbb95
abelvalle <abel@dsindigo.com>
2019-07-24 19:12:18 -0500

    Docker Config.

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,11 +1,18 @@
 # services/users/project/__init__.py
 
+import os
+import sys
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
 # instantiate the app
 app = Flask(__name__)
+# print(app.config, file=sys.stderr)
 api = Api(app)
+
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 # set config
 app.config.from_object('project.config.DevelopmentConfig') # new
